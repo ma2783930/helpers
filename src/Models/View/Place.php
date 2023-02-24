78b7e7c25956698ecdbf6476eab480ae04908b45
m.alizadeh <m.alizadeh@ip-jam.com>
2023-02-24 12:39:54 +0330

    removed continent_id in find scope

--- src/Models/View/Place.php
+++ src/Models/View/Place.php
@@ -62,8 +62,7 @@ class Place extends Model
      */
     public function scopeFor(Builder $builder, int $id): Builder
     {
-        return $builder->where('continent_id', $id)
-                       ->orWhere('country_id', $id)
+        return $builder->where('country_id', $id)
                        ->orWhere('province_id', $id)
                        ->orWhere('city_id', $id);
     }
