6ea4e541f5930870ac643b28602b01d4e40a4a2f
m.alizadeh <m.alizadeh@ip-jam.com>
2023-03-04 16:01:31 +0330

    added middleware for converting inputs to html special chars

---
+++ src/Http/Middleware/CleanInputs.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Helpers\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\TransformsRequest;
+use Closure;
+
+class CleanInputs extends TransformsRequest
+{
+
+    /**
+     * All of the registered skip callbacks.
+     *
+     * @var array
+     */
+    protected static array $skipCallbacks = [];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        foreach (static::$skipCallbacks as $callback) {
+            if ($callback($request)) {
+                return $next($request);
+            }
+        }
+
+        return parent::handle($request, $next);
+    }
+
+    /**
+     * Transform the given value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return string|null
+     */
+    protected function transform($key, $value): ?string
+    {
+        return !empty($value) ? htmlspecialchars($value) : null;
+    }
+
+    /**
+     * Register a callback that instructs the middleware to be skipped.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public static function skipWhen(Closure $callback): void
+    {
+        static::$skipCallbacks[] = $callback;
+    }
+}
