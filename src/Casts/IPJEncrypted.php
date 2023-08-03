1292fc28454cc403920fd66f5d2391df4c96075d
m.alizadeh <m.alizadeh@ip-jam.com>
2023-08-03 10:00:30 +0330

    fixed cast getter declaration

--- src/Casts/IPJEncrypted.php
+++ src/Casts/IPJEncrypted.php
@@ -2,14 +2,12 @@
 
 namespace Helpers\Casts;
 
-use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
-
 use Helpers\Facades\IPJEncryption;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class IPJEncrypted implements CastsAttributes
 {
-    public function get(Model $model, string $key, mixed $value, array $attributes)
+    public function get($model, string $key, $value, array $attributes)
     {
         if ($value === null) {
             return null;
@@ -18,7 +16,7 @@ class IPJEncrypted implements CastsAttributes
         return IPJEncryption::decrypt($value);
     }
 
-    public function set(Model $model, string $key, mixed $value, array $attributes)
+    public function set($model, string $key, mixed $value, array $attributes)
     {
         if ($value === null) {
             return null;
